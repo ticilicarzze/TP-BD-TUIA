051723b94d2838b9d8e04ed3fc5aca01193b676d
vitoramirez <107560512+vitoramirez@users.noreply.github.com>
2025-11-30 19:50:32 -0300

    Update Ejercicio3.sql

--- Ejercicio3.sql
+++ Ejercicio3.sql
@@ -1,4 +1,4 @@
-USE gestion_arbolado;
+USE TP_BBDD1_2025_GXX;
 GO
 
 -- ==============================================================================
@@ -168,3 +168,4 @@ INSERT INTO Reclamo (id_arbol, id_motivo, id_tarea, fecha, estado, fecha_asignac
 ('ARB-002', 4, NULL, '2025-11-08 16:00:00', 'pendiente', NULL);
 
 GO
+
