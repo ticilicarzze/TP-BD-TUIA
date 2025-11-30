22c4ab6bb59c8c40b025219eecef3d44897c0e4a
vitoramirez <107560512+vitoramirez@users.noreply.github.com>
2025-11-30 18:18:10 -0300

    Update Ejercicio5.sql
    corrección de ejercicio 5

--- Ejercicio5.sql
+++ Ejercicio5.sql
@@ -1,29 +1,37 @@
+USE gestion_arbolado
+
 -- 5.a
-CREATE VIEW VistaReclamo AS
+
+GO
+CREATE or ALTER VIEW VistaReclamo as
 SELECT
     r.id,
     r.fecha,
     r.id_arbol,
-    DATEDIFF(COALESCE(r.fecha_asignacion, NOW()), r.fecha) AS dias_asignacion,
-    DATEDIFF(COALESCE(t.fecha_ejecucion, NOW()), r.fecha) AS dias_resolucion
+    DATEDIFF(DAY, r.fecha, COALESCE(r.fecha_asignacion, GETDATE())) AS dias_asignacion,
+    DATEDIFF(DAY, r.fecha,  COALESCE(t.fecha_ejecucion, GETDATE())) AS dias_resolucion
 FROM Reclamo r
 LEFT JOIN Tarea t ON t.id = r.id_tarea;
+GO
 
 SELECT * FROM VistaReclamo ORDER BY dias_resolucion DESC;
 
 SELECT * FROM VistaReclamo WHERE dias_asignacion > 7;
 
 -- 5.b
-CREATE VIEW ResumenTareas AS
+
+GO
+CREATE or ALTER VIEW ResumenTareas AS
 SELECT 
     tt.descripcion AS tipo_tarea,
     MIN(t.fecha_ejecucion) AS fecha_primer_tarea,
     MAX(t.fecha_ejecucion) AS fecha_ultima_tarea,
     COUNT(t.id) AS cantidad
-FROM Tarea t
-JOIN Tipo_Tarea tt ON t.id_tipo = tt.id
+FROM Tarea AS t
+JOIN Tipo_Tarea AS tt ON t.id_tipo = tt.id_tipo
 GROUP BY tt.descripcion;
+GO
 
-SELECT * FROM ResumenTareas ORDER BY cantidad DESC LIMIT 10;
+SELECT TOP(10) * FROM ResumenTareas ORDER BY cantidad DESC;
 
 SELECT * FROM ResumenTareas WHERE cantidad > 5;
